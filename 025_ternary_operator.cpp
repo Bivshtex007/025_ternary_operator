505a638362e90f559b6a78d19241b6d7f4eb15e1
Bivshtex007 <103932684+Bivshtex007@users.noreply.github.com>
2024-07-31 02:06:13 +0300

    Add files via upload

---
+++ 025_ternary_operator.cpp
@@ -0,0 +1,14 @@
+using namespace std;
+#include <cmath>
+#include <iostream>
+
+int main() {
+
+int number;
+cout << "Enter INT number" << endl;
+cin >> number;
+( number % 2 == 1 ) ? cout << "Your number is ODD" : cout << "Your number is EVEN";
+
+return 0;
+
+}
